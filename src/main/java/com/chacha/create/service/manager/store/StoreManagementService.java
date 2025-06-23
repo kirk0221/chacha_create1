53e0784564d81c77cc6e2e9eee3b6e109dc3edbc
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-06-23 18:30:45 +0900

    🐛 fix: 컨트롤러에 적을걸 서비스로 적었어요....
    🐛 fix: 컨트롤러에 적을걸 서비스로 적었어요....

--- src/main/java/com/chacha/create/service/manager/store/StoreManagementService.java
+++ src/main/java/com/chacha/create/service/manager/store/StoreManagementService.java
@@ -2,24 +2,22 @@ package com.chacha.create.service.manager.store;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.chacha.create.common.entity.store.StoreEntity;
 import com.chacha.create.common.mapper.store.StoreMapper;
 
 @Service
-@RequestMapping("/manager")
 public class StoreManagementService {
 	
 	private StoreMapper storeMapper;
 	
+	@Autowired
 	StoreManagementService(StoreMapper storeMapper){
 		this.storeMapper = storeMapper;
 	}
 	
-	@GetMapping("/storelist")
 	public List<StoreEntity> storelist(){
 		return storeMapper.selectAll();
 	}
