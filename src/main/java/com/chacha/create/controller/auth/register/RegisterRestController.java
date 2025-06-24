e2e67c0734303eb0a4f77077fd220f3607bd42dc
kirk0221 <99606072+kirk0221@users.noreply.github.com>
2025-06-24 11:06:38 +0900

    ✨ feat: 회원가입 후 바로 로그인 추가
    ✨ feat: 회원가입 후 바로 로그인 추가 및 스토어 아이디 자동 생성

--- src/main/java/com/chacha/create/controller/auth/register/RegisterRestController.java
+++ src/main/java/com/chacha/create/controller/auth/register/RegisterRestController.java
@@ -1,5 +1,7 @@
 package com.chacha.create.controller.auth.register;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,15 +24,18 @@ public class RegisterRestController {
 	private RegisterService registerService;
 	
 	@PostMapping(value = "/userinfo", produces = MediaType.APPLICATION_JSON_VALUE)
-	public int userinfo(@RequestBody MemberEntity memberEntity) {
-		int result = registerService.memberinsert(memberEntity);
-		log.info("RegisterRestController에서 Member에 "+result + "개 입력");
-		return result;
+	public MemberEntity userinfo(HttpSession session, @RequestBody MemberEntity memberEntity) {
+		MemberEntity member = null;
+		member = registerService.memberinsert(memberEntity);
+		log.info("member 정보 입력 완료 : " + member.toString());
+		session.setAttribute("loginMember", member);
+		return member;
 	}
 	
 	@PostMapping(value = "/seller", produces = MediaType.APPLICATION_JSON_VALUE)
-	public int seller(@RequestBody SellerEntity sellerEntity) {
-		int result = registerService.sellerinsert(sellerEntity);
+	public int seller(HttpSession session, @RequestBody SellerEntity sellerEntity) {
+		MemberEntity member = (MemberEntity) session.getAttribute("loginMember");
+		int result = registerService.sellerinsert(sellerEntity, member);
 		log.info("RegisterRestController에서 Seller에 "+result + "개 입력");
 		return result;
 	}
