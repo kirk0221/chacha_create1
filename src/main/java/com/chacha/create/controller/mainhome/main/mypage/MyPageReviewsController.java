c61e95a5e34436382b4f32e6594d7e7be096401a
jahuiLee <jennysky46@gmail.com>
2025-06-24 17:38:25 +0900

    ♻️ refactor: 리뷰 서비스 통일

--- src/main/java/com/chacha/create/controller/mainhome/main/mypage/MyPageReviewsController.java
+++ src/main/java/com/chacha/create/controller/mainhome/main/mypage/MyPageReviewsController.java
@@ -7,14 +7,13 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chacha.create.common.entity.member.MemberEntity;
 import com.chacha.create.common.entity.order.ReviewEntity;
-import com.chacha.create.service.buyer.mypage.MainhomeMypageReviewService;
+import com.chacha.create.service.buyer.detail.ReviewService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,32 +23,19 @@ import lombok.extern.slf4j.Slf4j;
 public class MyPageReviewsController {
 
     @Autowired
-    private MainhomeMypageReviewService mainhomeMypageService;
+    private ReviewService reviewService;
     
     // 리뷰 ID로 조회 (GET 방식) - 단건 조회
     @GetMapping(value = "/reviewById", produces = MediaType.APPLICATION_JSON_VALUE)
     public ReviewEntity getReviewById(@RequestParam int reviewId) {
-        return mainhomeMypageService.selectReviewById(reviewId);
+        return reviewService.selectByReviewId(reviewId);
     }
     
     // 회원 ID로 조회 - 내 리뷰 전체 조회
     @GetMapping(value = "/reviewmemberById", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ReviewEntity> getmemberId(HttpSession session) {
     	MemberEntity member= (MemberEntity) session.getAttribute("loginMember");
-    	return mainhomeMypageService.selectByMemberId(member.getMemberId());
+    	return reviewService.selectByMemberId(member.getMemberId());
     };
      
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
