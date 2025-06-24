698a927c1b433d4d42c29d7ee9c424c29e545556
sehyeon <nocount334@gmail.com>
2025-06-24 15:55:33 +0900

    ✨ feat: 스토어 구매자 전체상품 페이지 추가

--- src/main/java/com/chacha/create/common/mapper/product/StoreProductMapper.java
+++ src/main/java/com/chacha/create/common/mapper/product/StoreProductMapper.java
@@ -1,10 +1,11 @@
 package com.chacha.create.common.mapper.product;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.chacha.create.common.dto.product.StoreProductDTO;
+import com.chacha.create.common.entity.product.StoreProductEntity;
 
 /**
  * 스토어ID로 스토어의 인기상품을 조회
@@ -12,6 +13,8 @@ import com.chacha.create.common.dto.product.StoreProductDTO;
 @Mapper
 public interface StoreProductMapper {
 	
-	List<StoreProductDTO> selectForBestProduct(int storeId);
-	List<StoreProductDTO> selectForMainProduct(int storeId);
+	List<StoreProductEntity> selectBestProduct(int storeId);
+	List<StoreProductEntity> storemainProduct(int storeId);
+	List<StoreProductEntity> selectForProductList(Map<String, Object> params);
+	List<StoreProductEntity> selectByProductName(Map<String, Object> params);
 }
